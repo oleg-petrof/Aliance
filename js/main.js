0703cf5fb82872f717ab78dda444f4c94e73ebc8
oleg-petrof <petrof.oleg@vk.com>
2022-10-18 19:52:00 +0500

    fix: adaptive mobile header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
@@ -8,4 +10,7 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo-light";
   }
+});
+mMenuToggle.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open");
 });
