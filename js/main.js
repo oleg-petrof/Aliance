ab6d7c4349afbcc599a1e90240546a7e36775011
oleg-petrof <petrof.oleg@vk.com>
2022-10-23 20:06:00 +0500

    add: modal pop-up

--- js/main.js
+++ js/main.js
@@ -110,3 +110,18 @@ const swiperBlog = new Swiper(".blog-slider", {
     },
   },
 });
+
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+console.log(modalToggle);
+modalToggle.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
